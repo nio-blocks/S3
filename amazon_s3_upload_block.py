c3e17e157872ebf72d1da985eee49b501d1d2909
cowleyk <cowley.kevin.l@gmail.com>
2017-07-12 13:47:24 -0600

    Refactor to have stand-alone upload and download blocks

--- amazon_s3_upload_block.py
+++ amazon_s3_upload_block.py
@@ -1,23 +1,44 @@
-from nio.properties import (VersionProperty, StringProperty,
+import boto3
+
+from nio.block.base import Block
+from nio.signal.base import Signal
+from nio.properties import (VersionProperty, StringProperty, PropertyHolder,
                             ObjectProperty)
-from .amazon_base_block import AmazonBase, AWSCreds
-from nio.util.discovery import discoverable
 
 
-@discoverable
-class AmazonS3(AmazonBase):
+class AWSCreds(PropertyHolder):
+    aws_access_key_id = StringProperty(
+        title="Access Key ID", default="", allow_none=False)
+    aws_secret_access_key = StringProperty(
+        title="Secret Access Key", default="", allow_none=False)
+    aws_session_token = StringProperty(
+        title="Session Token", default="", allow_none=False)
+
+
+class AmazonS3Upload(Block):
 
     version = VersionProperty("1.0.0")
     creds = ObjectProperty(
         AWSCreds, title="AWS Credentials", default=AWSCreds())
 
-    # Should these properties be in the base block?
     # Path to file on local machine
-    file_name = StringProperty(title="S3 File Name", default="")
+    file_name = StringProperty(title="File to Upload", default="")
     # S3 bucket to upload to
-    bucket_name = StringProperty(title="Bucket Name", default="bucket")
+    bucket_name = StringProperty(title="Bucket Name", default="")
     # What to name file in S3 bucket
     key = StringProperty(title="S3 File Key", default="")
+
+    def __init__(self):
+        self.client = None
+        super().__init__()
+
+    def configure(self, context):
+        super().configure(context)
+        self.client = boto3.client(
+            's3',
+            aws_access_key_id=self.creds().aws_access_key_id(),
+            aws_secret_access_key=self.creds().aws_secret_access_key(),
+            aws_session_token=self.creds().aws_secret_access_key())
 
     def process_signals(self, signals):
         for signal in signals:
@@ -30,7 +51,7 @@ class AmazonS3(AmazonBase):
                     self.key(signal))
             except:
                 self.logger.exception("File upload failed")
-            # self.notify_signals
+        # self.notify_signals
 
     # How does the relative path stuff work?
         # How will nio access file to upload?
--- tests/test_amazonS3_block.py
+++ tests/test_amazonS3_block.py
@@ -1,15 +1,20 @@
+from unittest.mock import patch
+
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..amazon_base_block import AmazonBase, AWSCreds
+
+from ..amazon_s3_upload_block import AmazonS3Upload
+from ..amazon_s3_download_block import AmazonS3Download
 
 
-class TestExample(NIOBlockTestCase):
+class TestAmazonS3(NIOBlockTestCase):
 
-    def test_process_signals(self):
+    def test_upload_file(self):
         """Signals pass through block unmodified."""
-        blk = Example()
-        self.configure_block(blk, {})
+        blk = AmazonS3Upload()
+        # with patch.object(blk,
+            self.configure_block(blk, {})
         blk.start()
         blk.process_signals([Signal({"hello": "n.io"})])
         blk.stop()
